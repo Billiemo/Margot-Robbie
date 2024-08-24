5006e12d64e292bbb46c2e0a6c6ad0f35dbb49fb
Billiemo <mokirpolk@gmail.com>
2024-08-24 19:19:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const img = document.createElement('img');
-    img.src = 'https://path-to-your-image.com/image.jpg'; // Replace with your image URL
+    img.src = 'https://cdn.sextfun.com/original/4X/2/c/7/2c752f3554b60f7443bb03c0a65a09e6332fd279.jpeg'; // Replace with your image URL
     img.alt = 'Dynamic Image';
     document.body.appendChild(img);
 });
